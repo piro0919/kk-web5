c28ba354442ec3ce6911ebe9603628a726cb5921
kouhei.kawamura <piro.haniwa@gmail.com>
2024-02-07 16:44:44 +0900

    feat: add bluesky link

--- src/libs/links/index.ts
+++ src/libs/links/index.ts
@@ -2,6 +2,7 @@ const links = [
   "https://github.com/piro0919",
   "https://www.instagram.com/piro0919",
   "https://twitter.com/piro0919",
+  "https://bsky.app/profile/piro0919.bsky.social",
 ];
 
 export default links;
