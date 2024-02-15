6e88ebe5d090c7974804618da2feecd0b3b4b545
piro <piro.haniwa@gmail.com>
2024-02-15 09:11:20 +0900

    feat: update bluesky link

--- src/libs/links/index.ts
+++ src/libs/links/index.ts
@@ -2,7 +2,7 @@ const links = [
   "https://github.com/piro0919",
   "https://www.instagram.com/piro0919",
   "https://twitter.com/piro0919",
-  "https://bsky.app/profile/piro0919.bsky.social",
+  "https://bsky.app/profile/kk-web.link",
 ];
 
 export default links;
