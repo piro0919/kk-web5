eaebf13ee5d517449a8f60825bf2f2582113f6cc
piro0919 <piro.haniwa@gmail.com>
2024-08-23 14:57:13 +0900

    feat: remove bluesky

--- src/libs/links/index.ts
+++ src/libs/links/index.ts
@@ -2,7 +2,6 @@ const links = [
   "https://github.com/piro0919",
   "https://www.instagram.com/piro0919",
   "https://twitter.com/piro0919",
-  "https://bsky.app/profile/kk-web.link",
 ];
 
 export default links;
