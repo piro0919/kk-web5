1c7c38878ddfbab80ad58659b2d028efcc2d0f95
piro0919 <piro.haniwa@gmail.com>
2024-10-30 06:15:46 +0900

    feat: add blog

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 });
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    dirs: ["src"],
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
   },
